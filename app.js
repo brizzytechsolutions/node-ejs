8c35488e920990645f7b3d0326963a95a2d7b7ff
Kabza Ngwana <k.cheese.b@gmail.com>
2021-06-18 15:13:51 +0200

    Done with Auth

---
+++ app.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const cors = require('cors');
+// const port = 4000;
+const app = express();
+const LocalStrategy = require('passport-local');
+const passportLocalMongoose = require('passport-local-mongoose');
+const User = require('./models/user');
+
+app.use(cors());
+
+mongoose.connect('mongodb://localhost/auth_demo_app', {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(
+  require('express-session')({
+    secret: 'Any normal Word', //decode or encode session
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// app.listen(port, () => {
+//     console.log(`Application listening on port ${port}`);
+// });
+
+app.use(
+  require('express-session')({
+    secret: 'Any normal Word', //decode or encode session
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+passport.serializeUser(User.serializeUser()); //session encoding
+passport.deserializeUser(User.deserializeUser()); //session decoding
+passport.use(new LocalStrategy(User.authenticate()));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+//=======================
+//      R O U T E S
+//=======================
+app.get('/', (req, res) => {
+  res.render('home');
+});
+app.get('/userprofile', isLoggedIn, (req, res) => {
+  res.render('userprofile');
+});
+//Auth Routes
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+app.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/userprofile',
+    failureRedirect: '/login',
+  }),
+  function (req, res) {}
+);
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+app.post('/register', (req, res) => {
+  User.register(
+    new User({
+      username: req.body.username,
+      phone: req.body.phone,
+      telephone: req.body.telephone,
+    }),
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.render('register');
+      }
+      passport.authenticate('local')(req, res, function () {
+        res.redirect('/login');
+      });
+    }
+  );
+});
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+//Listen On Server
+app.listen(process.env.PORT || 3000, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Server Started At Port 3000');
+  }
+});
